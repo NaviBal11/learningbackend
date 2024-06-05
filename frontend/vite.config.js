0246d3d51d53e9f4e9975d66eefcaa9d70fc63ed
Navi <navikainth11@gmail.com>
2024-06-05 12:41:33 -0600

    last change i wish

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://learningbackend-be.vercel.app/api",
-      changeOrigin: true,
-      secure: false,
-      rewrite: (path) => path.replace(/^\/api/, ""),
+      "/api": {
+        target: "https://learningbackend-be.vercel.app/api",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
   plugins: [react()],
